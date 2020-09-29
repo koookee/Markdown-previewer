335092925ba9b8931d84f25cd5e7a80ce94567c7
koookee <55768904+koookee@users.noreply.github.com>
2020-09-29 03:27:22 -0400

    Passed the optional user story (carriage returns). Updated ReadMe file. Attempting to add CSS to blockquotes, table, and background color

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-
+import styles from '../CSS/preview.module.css';
 let marked = require("marked");
-
+marked.setOptions({
+  breaks: true
+})
 class Preview extends Component{
   constructor(){
     super();
@@ -12,7 +14,7 @@ class Preview extends Component{
         <div style={{backgroundColor:""}}>
           <h2>Preview</h2>
           <div style={{backgroundColor:"", borderStyle:"solid"}}>
-            <div id="preview" dangerouslySetInnerHTML = {{__html:marked(this.props.name)}}>
+            <div id="preview" class={styles.preview} dangerouslySetInnerHTML = {{__html:marked(this.props.name)}}>
             </div>
           </div>
         </div>
